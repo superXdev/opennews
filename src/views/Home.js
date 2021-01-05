f1d480f7e3eb915ed3a5730b96568939ef8d797c
Fikri Rudiansyah <fikrizhanking@gmail.com>
2021-01-05 14:24:18 +0700

    Adding loading animation

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,10 +1,16 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
+import { BeatLoader } from 'react-spinners'
+import { css } from '@emotion/react'
 
 import Sidebar from '../components/Sidebar'
 import Article from '../components/Article'
 import Header from '../components/Header'
 import Paginate from '../components/Paginate'
+
+const loaderCSS = css`
+	color: black;
+`
 
 function Home() {
 	const [articles, setArticles] = useState([])
@@ -12,16 +18,19 @@ function Home() {
 	const [page, setPage] = useState(1)
 	const [maxPage, setMaxPage] = useState(0)
 	const [keyword, setKeyword] = useState('')
+	const [loading, setLoading] = useState(false)
 
 	const handleChangeOrder = (e) => {
 		setOrder(e.target.value)
 	}
 
 	const getArticles = async () => {
+		setLoading(true)
 		try {
 			let response = await axios.get(`${process.env.REACT_APP_API_URL}/api/news?order_by=${order}&limit=15&page=${page}&keyword=${keyword}`)
 			setArticles(response.data.results)
 			setMaxPage(response.data.pages)
+			setLoading(false)
 		} catch(e) {
 			console.log(e.message)
 		}
@@ -53,6 +62,10 @@ function Home() {
 				keyword={keyword}
 				setKeyword={setKeyword}
 				/>
+
+				<div className="text-center">
+				<BeatLoader css={loaderCSS} size={24} color='darkblue' loading={loading} />
+				</div>
 					
 				{
 					articles.map(article => {
@@ -70,11 +83,12 @@ function Home() {
 					})
 				}
 
-				<Paginate 
-				handlePage={handlePage}
-				page={page}
-				maxPage={maxPage}
-				/>
+				{ (loading) ? '' : (<Paginate 
+					handlePage={handlePage}
+					page={page}
+					maxPage={maxPage}
+					/>) }
+				
 					
 				</div>
 				<div className="col-md-4">
